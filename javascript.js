6eb559309b2cf346623b2a54e53c7abd7fdb100e
brendan_jackson <bajackson859@gmail.com>
2015-10-20 19:46:05 -0400

    added descriptive text

--- javascript.js
+++ javascript.js
@@ -1,7 +1,19 @@
 function buttonClicked() {
     var myText = document.getElementById('myText');
     var inputtext = document.getElementById('inputtext').value  ;
-        myText.innerHTML += "<li>"+inputtext+"</li>";
+    var inputdate = document.getElementById('inputdate').value  ;
+        myText.innerHTML += "<li>"+inputtext+" "+inputdate+"</li>";
+
     
 }
 
+/*
+how do I make this recorded text turn into its own object
+*/
+function task(text, date, time)
+
+/*
+input text is goes 
+var task = new task(text, date, time)
+
+*/
