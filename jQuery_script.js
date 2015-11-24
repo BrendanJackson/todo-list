21c0f69665f4da9846ccceda767cca0141cd7c94
brendan_jackson <bajackson859@gmail.com>
2015-11-24 18:16:12 -0500

    bootstrap additions

--- jQuery_script.js
+++ jQuery_script.js
@@ -8,7 +8,8 @@ $(document).ready(function() {
        input_text = $('#input_text').val();
        input_date = $('#input_date').val();
        input_time = $('#input_time').val();
-       options_menu = '<select id="select"> <option value="daily">Daily</option> <option value="critical">Critical</option> <option value="important">Important</option> <option value="future_importance">Future Importance</option> </select> ';
+       options_menu = '<select class="form-control" id="sel1"> <option value="daily">Daily</option> <option value="critical">Critical</option> <option value="important">Important</option> <option value="future_importance">Future Importance</option> </select>';
+   
        
       //http://stackoverflow.com/questions/1221957/jquery-to-retrieve-and-set-selected-option-value-of-html-select-element
 
@@ -16,7 +17,33 @@ $(document).ready(function() {
   
 });
 
+/*
+//how do I make this send to another list on submit? replace alert.
+  //http://www.theextremewebdesigns.com/blog/jquery-get-selected-index-jquery-get-selected-option-index-2-ways/
+  //similar possibly better http://jsfiddle.net/Nj85e/
+  $('#get').click(function() {
+ get = '<input type="submit" name="get" id="get" value="Get selected index" />'
+  
+     $('.daily').text("index#" +  );
+  
+  alert( $("#select option:selected").index() );
+ 
+    });
+*/
 
+});
+
+
+
+
+/*
+  1)I want to turn input_text/date/time into a single object
+    *kind of like (input_text + input_date + input_time) = wellness_object# see .extend, not extend
+  2)I want to make that object clickable
+  3)I want to make that click open a menu with the abillty to send that object to tables critical, important, or future importance 
+*/
+   	
+    
   /* try this instead
    $("#buttonClicked").on("click", function(e){
     e.preventDefault(); //this, along with return false, will prevent default behavior from button clicks - probably only useful in web applications, but a good convention to start using
@@ -37,26 +64,3 @@ $(document).ready(function() {
 }
 */
 
-
-$('#get').click(function() {
- get = '<input type="submit" name="get" id="get" value="Get selected index" />'
- 
-  //how do I make this send to another list on submit? replace alert.
-  //http://www.theextremewebdesigns.com/blog/jquery-get-selected-index-jquery-get-selected-option-index-2-ways/
-  alert( $("#select option:selected").index() );
- 
-  
-});
- 
-
-
-});
-
-/*
-  1)I want to turn input_text/date/time into a single object
-    *kind of like (input_text + input_date + input_time) = wellness_object# see .extend, not extend
-  2)I want to make that object clickable
-  3)I want to make that click open a menu with the abillty to send that object to tables critical, important, or future importance 
-*/
-   	
-    
