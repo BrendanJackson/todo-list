66d97b608040472943a9c686e96968dde3825eee
brendan_jackson <bajackson859@gmail.com>
2016-03-08 21:32:52 -0500

    fixed error, learned more about .data()

--- attempt2.js
+++ attempt2.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
                       variabes
     ===========================================*/
   var task 
-  var datetimepicker = $('.datetimepicker').val();
+  var datetimepicker 
   
   var set1_menu 
   var set1_index  
@@ -13,6 +13,8 @@ $(document).ready(function() {
   var set2_index
   var set2_button
  
+/*jQuery('#datetimepicker').datetimepicker();*/
+
 
 /*==================================================================================================================
 ==================================================================================================================*/
@@ -30,9 +32,9 @@ $(document).ready(function() {
       task = $("#input_text").val();
      	set1_menu = $(".set1_menu option:selected");
       set1_index = set1_menu.index() //switch arg
-
+      datetimepicker = $('#datetimepicker').datetimepicker();
       set2_menu = '<select class="set2_menu"> <option value="set2_table0">set2table0</option> <option value="set2_table1">set2table1</option> <option value="set2_table2">set2table2</option> <option value="set2_table3">set2table3</option> </select>';
-      set2_button = '<input type="button" class="set2_button" data-from="'+set1_index+'" value="set2_button" />'
+      set2_button = '<input type="button" class="set2_button" data-set1_data="'+set1_index+'" value="set2_button" />'
   	  
       switch(set1_index){
         /*===================  append <li> with no style type  ========================*/
@@ -62,17 +64,14 @@ $(document).ready(function() {
 ==================================================================================================================*/
 /*http://stackoverflow.com/questions/12065329/jquery-adding-event-listeners-to-dynamically-added-elements*/
  /*check gibberish's answer*/
-  $(document).on("click", "[class=set2_button]", function()   {  
-  /*$("div.set1.row").on("click", "input.set2_button", function(){*/ 
+  /*$(document).on("click", "[class=set2_button]", function()   {*/  
+  $("div.set1.row").on("click", "input.set2_button", function(){ 
       /*==========================================
                         variabes
       ===========================================*/
           
       var set2_index = $(this).closest('li').find('select.set2_menu option:selected').index();
-          
-
-
-
+      var set1_data = $(this).data('set1_data');
       
       /*=========================================
                         POST
@@ -83,30 +82,52 @@ $(document).ready(function() {
       =========================================*/
       console.log( "IT WORKS" + " " + set2_index );
 
-   switch(set1_index){
-        /*===================  append <li> with no style type  ========================*/
+   switch(set2_index){
+        /*===================  append <li> with no style type, consider consolidating append info into a variable  ========================*/
         case 0:
-        $('.set2_table0').append("<li>" + task + " " /*+ datetimepicker + " "*/  + "</li>" );
+        $('.set2_table0').append("<li>" + task + " (from Table " +set1_data+ ")" + /*and from set2*/" " /*+ datetimepicker + " "*/  + "</li>" );
+        $('.master').append( "<li>" + task + " (from Table " +set1_data+ ")" /*and from set2*/+ " "/* + datetimepicker + " "*/ + "</li>" );
         break;
 
          case 1:
-        $('.set2_table1').append("<li>" + task + " " /*+ datetimepicker + " "*/  + "</li>" );
+        $('.set2_table1').append("<li>" + task + " (from Table " +set1_data+ ")" + /*and from set2*/" " /*+ datetimepicker + " "*/  + "</li>" );
+        $('.master').append( "<li>" + task + " (from Table " +set1_data+ ")" /*and from set2*/+ " "/* + datetimepicker + " "*/ + "</li>" );
         break;
 
          case 2:
-        $('.set2_table2').append( "<li>" + task + " "/* + datetimepicker + " "*/ + "</li>" );
+        $('.set2_table2').append( "<li>" + task + " (from Table " +set1_data+ ")" + /*and from set2*/" "/* + datetimepicker + " "*/ + "</li>" );
+        $('.master').append( "<li>" + task + " (from Table " +set1_data+ ")" /*and from set2*/+ " "/* + datetimepicker + " "*/ + "</li>" );
         break;
 
          case 3:
-        $('.set2_table3').append( "<li>" + task + " "/* + datetimepicker + " "*/ + "</li>" );
+        $('.set2_table3').append( "<li>" + task + " (from Table " +set1_data+ ")" /*and from set2*/ + " "/* + datetimepicker + " "*/ + "</li>" );
+        $('.master').append( "<li>" + task + " (from Table " +set1_data+ ")" /*and from set2*/+ " "/* + datetimepicker + " "*/ + "</li>" );
         break;
     }
    
-
-
-
   });
+
+/*=====================================================================================================
+{MASTER
+====Append vs appendTo=======https://www.codecademy.com/forum_questions/50031633dd2be800020302b5
+}
+======================================================================================================*/
+
+
 
 });
 
 
+/*
+
+* datepicker with notes
+>> master list
+  -master list chronological order dependent on datepicker
+
+* this is from "x" list
+  - <li> styles
+
+
+1)which will have the biggest impact? datepicker
+
+*/
